cee58a3454bc970361db5ade008b221678039029
m.laic <m.laic@hotmail.com>
2024-03-16 21:28:13 +0100

    added code for creating a retrofit to use the url adress to server  in RetrofitRepository

--- app/src/main/java/com/example/examapp/retrofit/RetrofitRepository.kt
+++ app/src/main/java/com/example/examapp/retrofit/RetrofitRepository.kt
@@ -4,7 +4,25 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitRepository {
+    private val urlAddress = "http://192.168.50.253:9000"
+    private lateinit var retrofit: Retrofit
+    private lateinit var userApi: UserApi
 
-  
+    fun retrofitService(): Retrofit {
+
+        initializeRetrofit()
+       userApi = retrofit.create(UserApi::class.java)
+        return retrofit
+    }
+
+    private fun initializeRetrofit() {
+        retrofit = Retrofit.Builder()
+            .baseUrl(urlAddress)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+
+    }
+
 
 }
